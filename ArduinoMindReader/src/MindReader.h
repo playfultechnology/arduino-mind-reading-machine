36ad66a012d88f333dc5a166bd2c6dffa0c03e0f
alastaira <alastair.aitchison@gmail.com>
2022-11-23 06:24:46 +0000

    More experts added

--- ArduinoMindReader/src/MindReader.h
+++ ArduinoMindReader/src/MindReader.h
@@ -17,15 +17,14 @@ class MindReader {
 		void updateBotPrediction();	
 	
 		int8_t *m_userMoves;   //holds past user moves
-    int8_t *m_userMovesFlipping;   //holds past user moves in the flipping case (if current=last then 1 else -1)
-    int8_t *m_botMoves;    //holds past bot moves  (including the current prediction, so its size is always gameTurn+1)
-    int8_t *m_wins;        // holds the history of who won
+		int8_t *m_userMovesFlipping;   //holds past user moves in the flipping case (if current=last then 1 else -1)
+		int8_t *m_botMoves;    //holds past bot moves  (including the current prediction, so its size is always gameTurn+1)
+		int8_t *m_wins;        // holds the history of who won
 		
 		Predictor *m_predictors;
-		
-		
-    uint8_t m_gameTurn = 0;    // current game turn - pointer to the head of these arrays.
-    uint8_t m_numberOfGameTurns;
+			
+		uint8_t m_gameTurn = 0;    // current game turn - pointer to the head of these arrays.
+		uint8_t m_numberOfGameTurns;
 };
 
 #endif
--- ArduinoMindReader/src/Predictor.cpp
+++ ArduinoMindReader/src/Predictor.cpp
@@ -37,47 +37,138 @@ int8_t Predictor::makePrediction(int8_t* userMoves, int8_t* userMovesFlipping, i
 			cnt +=1;
 		}
 		historyMean /= cnt;
-		return historyMean;	
+		return historyMean * userMoves[numTurns-1] * -1; // flip or not the last user move;	
 	}
 	else if(m_predictorType == PredictorType::PATTERN && m_dataType == DataType::REGULAR_DATA_SERIES) {
-/*
-        var pattern, prediction, score, ind;
-
-        if (data.length < this.memoryLength) {
-          return 0;
-        }
-
-        function rotatePattern() {
-					// removes the last element from an array and returns that element.
-            var temp = pattern.pop();
-						// adds new elements to the beginning of an array
-            pattern.unshift(temp);
-        }
-
-        //extract history length
-				// returns selected elements in an array, as a new array. The slice() method selects from a given start, up to a (not inclusive) given end
-        pattern = data.slice(-this.memoryLength);
-        prediction = pattern[0];  //the prediction is simply the element in the pattern (i.e. the first in this extracted array)
-
-
-        score = 0;
-        ind = data.length-this.memoryLength-1;  //start right before the pattern
-        while (ind>=Math.max(0, data.length-3*this.memoryLength)) { //check maximum 2 appearances of the full pattern
-            if (pattern[pattern.length-1]==data[ind]) {
-              score++;
-            }
-            rotatePattern()
-            ind--;
-        }
-        score /= (2*this.memoryLength);  //the maximum score is achieved when the pattern repeats itself twice
-        return prediction * score;
-				*/
+		
+		if(numTurns < m_memoryLength) {
+			return 0;
+		}
+		
+		// Retrieve the most recent entries from the array of data
+		uint8_t pattern[m_memoryLength];
+		for(int i=0; i<m_memoryLength; i++){
+			pattern[i] = userMoves[numTurns - m_memoryLength - 1 + i];
+		}			
+		
+		uint8_t prediction = pattern[0];
+		
+		uint8_t score = 0;
+		int i = numTurns - m_memoryLength - 1;
+		
+		while(i >= max(0, numTurns-3*m_memoryLength)) {
+			if(pattern[m_memoryLength-1] == userMoves[i]){
+				score++;
+			}
+			// Take the last element off the end of the array
+			uint8_t temp = pattern[m_memoryLength-1];
+			// Shift all the elements back one
+			for(int i=m_memoryLength-1; i>0; i--){
+				pattern[i] = pattern[i-1];
+			}
+			// And then put the last element back on the beginning
+			pattern[0] = temp;
+			
+			i--;
+		}
+		// The maximum score is achieved when the pattern repeats itself twice
+		score /= (2*m_memoryLength); 
+		return prediction * score;
 	}
 	else if(m_predictorType == PredictorType::PATTERN && m_dataType == FLIPPING_DATA_SERIES) {  //calculate the mean of the last memoryLength moves
-		;//prediction = this.childPredictor(userMovesFlipping) * userMoves[userMoves.length-1] * -1; // flip or not the last user move
+
+		if(numTurns < m_memoryLength) {
+			return 0;
+		}
+		
+		// Retrieve the most recent entries from the array of data
+		uint8_t pattern[m_memoryLength];
+		for(int i=0; i<m_memoryLength; i++){
+			pattern[i] = userMovesFlipping[numTurns - m_memoryLength - 1 + i];
+		}			
+		
+		uint8_t prediction = pattern[0];
+		
+		uint8_t score = 0;
+		int i = numTurns - m_memoryLength - 1;
+		
+		while(i >= max(0, numTurns-3*m_memoryLength)) {
+			if(pattern[m_memoryLength-1] == userMovesFlipping[i]){
+				score++;
+			}
+			// Take the last element off the end of the array
+			uint8_t temp = pattern[m_memoryLength-1];
+			// Shift all the elements back one
+			for(int i=m_memoryLength-1; i>0; i--){
+				pattern[i] = pattern[i-1];
+			}
+			// And then put the last element back on the beginning
+			pattern[0] = temp;
+			
+			i--;
+		}
+		// The maximum score is achieved when the pattern repeats itself twice
+		score /= (2*m_memoryLength); 
+		return prediction * score * userMoves[numTurns-1] * -1; // flip or not the last user move;	
 	}
   else if(m_predictorType == PredictorType::REACTIVE && m_dataType == USER_REACTIVE) {  //calculate the mean of the last memoryLength moves
 		;//prediction = this.childPredictor(userMovesFlipping, wins) * userMoves[userMoves.length-1] * -1; // flip or not the last user move
+		/*
+		constructor(memoryLength, dataType) {
+        super(memoryLength, dataType);
+        this.stateMachine = new Array(Math.pow(2, 2*memoryLength-1)).fill(0);
+        this.indMap = [];
+        for (var i=2*memoryLength; i>=0; i--) {
+          this.indMap.push(Math.pow(2, i));
+        }
+    }
+    childPredictor(moves, wins) {
+        // figure out what was the last state
+        var partOfMoves = moves.slice(moves.length - this.memoryLength    , moves.length - 1);
+        var partOfWins  = wins.slice (wins.length  - this.memoryLength - 1 , wins.length - 1);
+        var lastState = partOfWins.concat(partOfMoves);
+        var lastStateInd = 0
+        for (var i=0; i<lastState.length; i++) {
+            if (lastState[i]==1) {
+                lastStateInd += Math.pow(2,i);
+            }
+        }
+        var lastStateResult = moves[moves.length-1];
+
+        //update the state machine
+        if (this.stateMachine[lastStateInd] == 0) { //no prior info
+            this.stateMachine[lastStateInd] = lastStateResult*0.3;
+        }
+        else if (this.stateMachine[lastStateInd] == lastStateResult*0.3) {  //we've been here before so strengthen prediction
+            this.stateMachine[lastStateInd] = lastStateResult*0.8;
+        }
+        else if (this.stateMachine[lastStateInd] == lastStateResult*0.8) { //we've been here before so strengthen prediction
+            this.stateMachine[lastStateInd] = lastStateResult*1;
+        }
+        else if (this.stateMachine[lastStateInd] == lastStateResult*1) { //maximum confidence
+            this.stateMachine[lastStateInd] = lastStateResult*1;
+        }
+        else {  //changed his mind - so go back to 0
+            this.stateMachine[lastStateInd] = 0;
+        }
+
+        // what is the current state
+        var currentPartOfMoves = moves.slice(moves.length - this.memoryLength + 1    , moves.length);
+        var currentPartOfWins  = wins.slice (wins.length  - this.memoryLength , wins.length);
+        var currentState = currentPartOfWins.concat(currentPartOfMoves);
+        var currentStateInd = 0
+        for (var i=0; i<currentState.length; i++) {
+            if (currentState[i]==1) {
+                currentStateInd += Math.pow(2,i);
+            }
+        }
+
+        var predictionAndScore = this.stateMachine[currentStateInd]
+
+      //  console.log('last state ', lastState, ', last ind ', lastStateInd, '    current state ', currentState, '  current ind ', currentStateInd, '    state machine: ', this.stateMachine);
+        return predictionAndScore;
+		*/
+		
 	}
 	else if (m_predictorType == PredictorType::REACTIVE && m_dataType == USER_REACTIVE_REG_DATA) {  //calculate the mean of the last memoryLength moves
 		;//prediction = this.childPredictor(userMoves, wins);
