debd99bf913aa39d9859396fdd3ec26c756bb030
alastaira <alastair.aitchison@gmail.com>
2022-11-30 16:16:27 +0000

    Update

--- ArduinoMindReader/ArduinoMindReader.ino
+++ ArduinoMindReader/ArduinoMindReader.ino
@@ -1,16 +1,24 @@
+// INCLUDES
+// Include the mind reader library
 #include "src/MindReader.h"
 
+// GLOBALS
+// Create mindreader for a game with the specified number of turns
 MindReader mindReader(50);
 
+// For testing only, create a sequence of dummy moves
 int dummyUserMoves[8] = {1, -1, -1, -1, 1, 1, 1, -1};
 
 void setup() {
 
+  // For debug output
   Serial.begin(115200);
   Serial.println(__FILE__ __DATE__);
   
+  // Initialise the mindreader
   mindReader.begin();
 
+  // Run over a series of test moves
   for(int i=0; i<8; i++){
     Serial.print(F("Prediction:"));
     Serial.print(mindReader.getPrediction());
--- ArduinoMindReader/src/MindReader.cpp
+++ ArduinoMindReader/src/MindReader.cpp
@@ -9,7 +9,6 @@
 
 // See https://web.media.mit.edu/~guysatat/MindReader/GuySatat_MindReader_FinalProject.pdf
 
-
 //MindReader::MindReader(int numberOfGameTurns) { }
 
 void MindReader::begin(){
@@ -25,8 +24,8 @@ void MindReader::resetBot() {
 	m_userMovesFlipping = new int8_t[m_numberOfGameTurns]{};
 	m_botMoves = new int8_t[m_numberOfGameTurns]{};
 	m_wins = new int8_t[m_numberOfGameTurns]{};
-	
-	m_predictors = new Predictor[18] {
+
+	m_predictors = new Predictor[NUM_PREDICTORS] {
 		Predictor(2, Predictor::PredictorType::BIAS, Predictor::DataType::REGULAR_DATA_SERIES),
 		Predictor(3, Predictor::PredictorType::BIAS, Predictor::DataType::REGULAR_DATA_SERIES),
 		Predictor(5, Predictor::PredictorType::BIAS, Predictor::DataType::REGULAR_DATA_SERIES),
@@ -48,69 +47,69 @@ void MindReader::resetBot() {
 	};	
 }
 
-
+/**
+ * Process information after every new move that the user makes
+ */
 void MindReader::updateUserMove(int8_t userMove){
 	// Update user move array
-  m_userMoves[m_gameTurn] = userMove;
-
-  // Update user flipping array
-  if(m_gameTurn > 0) { // Make sure the player has taken at least one turn
+	m_userMoves[m_gameTurn] = userMove;
+	// Update user flipping array
+	if(m_gameTurn > 0) { // Make sure the player has taken at least one turn
 		if(m_userMoves[m_gameTurn] == m_userMoves[m_gameTurn-1]) { // If new move is the same as their last move
 			m_userMovesFlipping[m_gameTurn] = 1;
-    }
-    else {
+		}
+		else {
 			m_userMovesFlipping[m_gameTurn] = -1;
-    }
-  }
-
-  // Update the "who won" array
-  if (userMove == getPrediction()) {
+		}
+	}
+	// Update the "who won" array
+	if(userMove == getPrediction()) {
 		m_wins[m_gameTurn] = BOTWIN;
-  }
-  else {
+	}
+	else {
 		m_wins[m_gameTurn] = USERWIN;
-  }
-
+	}
 	// Move to the next turn
-  m_gameTurn += 1;
-	
+	m_gameTurn += 1;
 	// Update the bot prediction for the next round
-  updateBotPrediction();
+	updateBotPrediction();
 }
 
-// Populate the best guess for the user's next move based on move history
+/**
+ * Populate the best guess for the user's next move based on move history
+ */
 void MindReader::updateBotPrediction(){
-	
-	uint8_t numPredictors = 26;
-	
+
 	double botPredictionProb = 0;
 	uint8_t botPrediction;
 	
 	// Calculate 𝜂 as √((log 𝑁)/(2𝑇−1))
-	double eta = sqrt(log(numPredictors) / (2*m_numberOfGameTurns - 1) );
+	double eta = sqrt(log(NUM_PREDICTORS) / (2*m_numberOfGameTurns - 1) );
 	
-  uint8_t expertPastAccuracy;
+	uint8_t expertPastAccuracy;
 	double expertWeight;
 	int8_t currentPrediction;
 	double denominator=0, numerator=0;
-  for (int expertInd=0; expertInd<numPredictors; expertInd++) {
+	// Loop over every expert
+	for(int expertInd=0; expertInd<NUM_PREDICTORS; expertInd++) {
 		expertPastAccuracy = m_predictors[expertInd].getPastAccuracy(m_userMoves, m_gameTurn);
-    expertWeight = exp(-1 * eta * expertPastAccuracy);
-    currentPrediction = m_predictors[expertInd].makePrediction(m_userMoves, m_userMovesFlipping, m_wins, m_gameTurn);
-    numerator += currentPrediction * expertWeight;
-    denominator += expertWeight;
-  }
-  botPredictionProb = numerator / denominator;
+		expertWeight = exp(-1 * eta * expertPastAccuracy);
+		currentPrediction = m_predictors[expertInd].makePrediction(m_userMoves, m_userMovesFlipping, m_wins, m_gameTurn);
+		numerator += currentPrediction * expertWeight;
+		denominator += expertWeight;
+	}
+	// Make a prediction based on the weighted average opinion of all the experts 
+	botPredictionProb = numerator / denominator;
 
 	// Rademacher distribution (https://en.wikipedia.org/wiki/Rademacher_distribution)
 	int8_t sample = random(-1, 1);
-  if (botPredictionProb < sample) {
+	if(botPredictionProb < sample) {
 		botPrediction = -1;
 	}
-	else {
+	else{
 		botPrediction = 1;
 	}
-
+	// Place the prediction into the array
 	m_botMoves[m_gameTurn] = botPrediction;
 }
 
